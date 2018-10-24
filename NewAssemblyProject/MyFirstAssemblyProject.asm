1cec7ef3de302868c807b9267551f2ccbcfdfabd
Brian Hart <brian.hart@xylogix.net>
2018-10-24 11:16:56 -0400

    24 Oct 2018 Got project to build for the first time

--- NewAssemblyProject/MyFirstAssemblyProject.asm
+++ NewAssemblyProject/MyFirstAssemblyProject.asm
@@ -15,6 +15,20 @@ ExitProcess	PROTO, dwExitCode:DWORD
 .code
 main PROC
 ; write your code here
+
+; 300.11 eax = 0xFF00, ebx = 0x8800.  Write commands to compare eax and ebx using just al and bl.
+
+mov		eax, 0000FF00h
+mov		ebx, 00008800h
+cmp		al,bl
+jne		lbl6
+mov		al,ah
+mov		bl,bh
+cmp		al,bl
+je 		lbl7
+
+lbl6:
+lbl7:
 INVOKE ExitProcess,0
 main ENDP
 
